b8057f07122015850f14ef90aea1de66f2cf27fe
Hugo Duksis <duksis@users.noreply.github.com>
2021-08-09 11:35:08 +0200

    Move custom dependecies
    to a repo controlled by HS

--- mix.exs
+++ mix.exs
@@ -27,9 +27,9 @@ defmodule Graphqx.MixProject do
       # plug is our web layer
       {:plug, "~> 1.6"},
       # this is the erlang graphql library providing query parsing and object resolvers
-      {:graphql, git: "https://github.com/Overbryd/graphql-erlang.git", branch: "develop"},
+      {:graphql, git: "https://github.com/titel-media/graphql-erlang.git", branch: "develop"},
       # use custom Poison JSON en/decoder, hence the override: true
-      {:poison, git: "https://github.com/Overbryd/poison.git", branch: "master", override: true},
+      {:poison, git: "https://github.com/titel-media/poison.git", branch: "master", override: true},
       # ex_doc is generating documentation for us
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
